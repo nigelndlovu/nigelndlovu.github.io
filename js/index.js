ec6d9de810ce474af575e04ce13d5f40be82eb97
nigelndlovu <ndl20004@byui.edu>
2021-05-01 11:35:26 +0200

    commit

--- js/index.js
+++ js/index.js
@@ -1,1 +1,6 @@
+var date = new Date();
+var year = date.getFullYear();
+document.querySelector("#getyear").textContent = year;
+
+
 document.querySelector("#lastmod").textContent = document.lastModified;
