a0eddb0d79032a58b879abe7d40677d829df3a7d
nigelndlovu <ndl20004@byui.edu>
2024-05-25 20:15:47 +0200

    added new files and made some edits

---
+++ forms/forms.js
@@ -0,0 +1,10 @@
+// 1)
+let selectedOption = genres.options[genres.selectedIndex];
+alert( selectedOption.value );
+
+// 2)
+let newOption = new Option("Classic", "classic");
+genres.append(newOption);
+
+// 3)
+newOption.selected = true;
